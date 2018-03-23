d4480d9141f6e2c3c58e009db29eb568e154fb84
Pradeepkumar <cgpradeepkumar@gmail.com>
2018-03-23 23:02:28 +0530

    file copy example

--- sampleApacheCamel/src/main/java/in/samples/apache/camel/CamelStarter.java
+++ sampleApacheCamel/src/main/java/in/samples/apache/camel/CamelStarter.java
@@ -1,10 +1,30 @@
 package in.samples.apache.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
 public class CamelStarter {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final String input = "file:E:\\Temp\\camelTest\\input";
+	private static final String output = "file:E:\\Temp\\camelTest\\output";
 
+	public static void main(String[] args) throws Exception {
+		CamelContext context = new DefaultCamelContext();
+
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				from(input).to(output);
+			}
+		});
+
+		context.start();
+
+		Thread.sleep(5 * 60 * 1000);
+
+		context.stop();
 	}
 
 }
