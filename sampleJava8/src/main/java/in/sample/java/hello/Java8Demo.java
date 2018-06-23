485f8170eba2eb478c2eb800221761a04efb2196
Pradeepkumar <cgpradeepkumar@gmail.com>
2018-06-23 18:50:57 +0530

    streams

--- sampleJava8/src/main/java/in/sample/java/hello/Java8Demo.java
+++ sampleJava8/src/main/java/in/sample/java/hello/Java8Demo.java
@@ -1,12 +1,15 @@
 package in.sample.java.hello;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Java8Demo {
 
 	public static void main(String arg[]) {
+		List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+		List<Integer> integers = Arrays.asList(1, 2, 13, 4, 15, 6, 17, 8, 19);
 		List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
 		System.out.println("List : " + strings);
 
@@ -21,5 +24,18 @@ public class Java8Demo {
 
 		String mergedString = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", "));
 		System.out.println("Merged String = " + mergedString);
+
+		System.out.println("numbers = " + numbers);
+		List<Integer> squaresList = numbers.stream().map(i -> i * i).distinct().collect(Collectors.toList());
+		System.out.println("Squared List = " + squaresList);
+
+		System.out.println("Integers = " + integers);
+		IntSummaryStatistics stats = integers.stream().mapToInt(x -> x).summaryStatistics();
+
+		System.out.println("Highest number in the list = " + stats.getMax());
+		System.out.println("Lowest number in the list = " + stats.getMin());
+		System.out.println("Count = " + stats.getCount());
+		System.out.println("Sum = " + stats.getSum());
+		System.out.println("Average = " + stats.getAverage());
 	}
 }
