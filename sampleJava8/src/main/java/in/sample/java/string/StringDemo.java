abb2cd00e8e14180d60466a60db1d5129a1664ba
Pradeepkumar <cgpradeepkumar@gmail.com>
2018-09-08 12:56:43 +0530

    testing feature branch C -1

--- sampleJava8/src/main/java/in/sample/java/string/StringDemo.java
+++ sampleJava8/src/main/java/in/sample/java/string/StringDemo.java
@@ -4,6 +4,7 @@ public class StringDemo {
     public static void main(String[] args) {
         String a = "Pradeep";
         String b = new String("Pradeep");
+        String c = new String("Pradeep");
 
         if (a == b) {
             System.out.println("a==b");
@@ -15,6 +16,18 @@ public class StringDemo {
             System.out.println("a.equals(b)");
         } else {
             System.out.println("not a.equals(b)");
+        }
+
+        if (a == c) {
+            System.out.println("a==c");
+        } else {
+            System.out.println("not a==c");
+        }
+
+        if (a.equals(c)) {
+            System.out.println("a.equals(c)");
+        } else {
+            System.out.println("not a.equals(c)");
         }
     }
 }
