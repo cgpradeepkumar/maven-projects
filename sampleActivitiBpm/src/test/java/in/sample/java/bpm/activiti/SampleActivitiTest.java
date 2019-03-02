4cecce15d3a84954b3ee77098b767195d5fb9526
Pradeepkumar <cgpradeepkumar@gmail.com>
2019-03-02 19:27:21 +0000

    activiti process initial

--- sampleActivitiBpm/src/test/java/in/sample/java/bpm/activiti/SampleActivitiTest.java
+++ sampleActivitiBpm/src/test/java/in/sample/java/bpm/activiti/SampleActivitiTest.java
@@ -1,7 +1,10 @@
 package in.sample.java.bpm.activiti;
 
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,9 +20,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = SampleActivitiWithSpringApplication.class)
 public class SampleActivitiTest {
 
+    private static final Logger LOGGER = Logger.getLogger(SampleActivitiTest.class);
+
     @Test
     public void givenXMLConfig_whenGetDefault_thenGotProcessEngine() {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         Assert.assertNotNull(processEngine);
+        Assert.assertNotNull(processEngine.getName());
+    }
+
+    @Test
+    public void givenBPMN_whenDeployProcess_thenDeployed() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        repositoryService.createDeployment().addClasspathResource("VacationRequest.bpmn20.xml").deploy();
+        LOGGER.info("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());
     }
 }
