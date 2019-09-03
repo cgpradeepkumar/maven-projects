14b3569e75c7b25a03a2dcf46f8e03edfd791721
Pradeepkumar <cgpradeepkumar@gmail.com>
2019-09-03 20:26:38 +0100

    bdd with cucumber

--- sampleActivitiBpm/src/main/java/in/sample/java/bpm/activiti/SampleActivitiWithSpringApplication.java
+++ sampleActivitiBpm/src/main/java/in/sample/java/bpm/activiti/SampleActivitiWithSpringApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * cgpradeepkumar
- * 21-02-2019
+ * cgpradeepkumar 21-02-2019
  */
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "in.sample.java.bpm")
 public class SampleActivitiWithSpringApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SampleActivitiWithSpringApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SampleActivitiWithSpringApplication.class, args);
+	}
 }
