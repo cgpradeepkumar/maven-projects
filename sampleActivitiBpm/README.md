a7f605226f81ceb8403e725ff6ca7ab048ec338c
Pradeepkumar <cgpradeepkumar@gmail.com>
2019-09-23 19:55:27 +0100

    h2-console configuration

--- sampleActivitiBpm/README.md
+++ sampleActivitiBpm/README.md
@@ -1,1 +1,4 @@
 mvn archetype:generate -DgroupId=in.sample.java.bpm.activiti -DartifactId=sampleActivitiBpm -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+jdbc:h2:~/test
+
--- sampleActivitiBpm/src/main/java/in/sample/java/bpm/activiti/SampleActivitiWithSpringApplication.java
+++ sampleActivitiBpm/src/main/java/in/sample/java/bpm/activiti/SampleActivitiWithSpringApplication.java
@@ -1,15 +1,27 @@
 package in.sample.java.bpm.activiti;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * cgpradeepkumar 21-02-2019
  */
 
+@Configuration
 @SpringBootApplication(scanBasePackages = "in.sample.java.bpm")
 public class SampleActivitiWithSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleActivitiWithSpringApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean<WebServlet> h2servletRegistration() {
+		ServletRegistrationBean<WebServlet> registrationBean = new ServletRegistrationBean<>(new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
 	}
 }
