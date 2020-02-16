121f02fc0d5bfac1188075e7cb6cc6b37e2b9909
Pradeepkumar <cgpradeepkumar@gmail.com>
2020-02-16 23:08:03 +0000

    initial feign service sample

---
+++ sample-feign-service/src/main/java/in/sample/java/feign/SampleServiceApplication.java
@@ -0,0 +1,13 @@
+package in.sample.java.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServiceApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(SampleServiceApplication.class, args);
+	}
+
+}
