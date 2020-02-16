a808826beb5a415255373dec2236eb971ae08551
Pradeepkumar <cgpradeepkumar@gmail.com>
2020-02-16 22:27:47 +0000

    initial feign client

---
+++ sample-feign-service-client/src/main/java/in/sample/java/feign/SampleClientApplication.java
@@ -0,0 +1,15 @@
+package in.sample.java.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SampleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleClientApplication.class, args);
+	}
+
+}
